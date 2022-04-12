f78031eb4ea9737457bc1034841e1f267292928b
Linh Do <khanhlinhdothi@gmail.com>
2022-04-12 12:58:37 +0700

    adding files

---
+++ responsive.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+  }
+}
